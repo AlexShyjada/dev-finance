2187782cd157a4bec09f01f782e0df703eb2b1c9
Alexandre Shyjada Sousa <alexandresousa@salturssa.pms.ad>
2021-04-13 11:56:25 -0300

    Implementando javascript no data-table do index.html

--- project/scripts.js
+++ project/scripts.js
@@ -12,3 +12,63 @@ const Modal = {
       .remove('active')
   }
 }
+
+const transaction = [
+  {
+  id: 1,
+  description: 'Luz',
+  amount: -50000,
+  date: '23/01/2021'
+  },
+  {
+  id: 2,
+  description: 'Website',
+  amount: 500000,
+  date: '23/01/2021'
+  },
+  {
+  id: 3,
+  description: 'Internet',
+  amount: -20000,
+  date: '23/01/2021'
+  },
+  {
+  id: 4,
+  description: 'App',
+  amount: 200000,
+  date: '23/01/2021'
+  }
+]
+
+const DOM = {
+
+  transactionContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index){
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionContainer.appendChild(tr)
+
+    
+  },
+
+  innerHTMLTransaction(transaction){
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+    // const amount
+
+    const html = `      
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${transaction.amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td><img src="img/minus.svg" alt="Remover transação"></td>
+    `
+
+    return html
+  }
+}
+
+transaction.forEach(function(transaction){
+  DOM.addTransaction(transaction)
+})
